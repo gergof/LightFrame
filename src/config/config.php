9f6ebe4e8c759e802728c4873504193c12b5930a
Fándly Gergő <gergo@systemtest.tk>
2018-11-03 17:52:04 +0200

    Fixed the overwrite of $config

--- src/config/config.php
+++ src/config/config.php
@@ -62,7 +62,7 @@ if($config["general"]["debug"]){
 //set up LoginMaster
 //uncomment these if you have LoginMaster installed
 /*
-$config=new \LoginMaster\Config($db, $config["login"]["session_lifetime"], $config["login"]["captcha_enable"], $config["login"]["captcha_after"], $config["login"]["captcha_sitekey"], $config["login"]["captcha_secretkey"], $config["login"]["ban_enable"], $config["login"]["ban_after"], $config["login"]["ban_time"], $config["login"]["look_time"], $config["login"]["remember_enable"], $config["login"]["remember_time"], "username");
+$lmconfig=new \LoginMaster\Config($db, $config["login"]["session_lifetime"], $config["login"]["captcha_enable"], $config["login"]["captcha_after"], $config["login"]["captcha_sitekey"], $config["login"]["captcha_secretkey"], $config["login"]["ban_enable"], $config["login"]["ban_after"], $config["login"]["ban_time"], $config["login"]["look_time"], $config["login"]["remember_enable"], $config["login"]["remember_time"], "username");
 class lmHandler implements \LoginMaster\Handler{
     public function handle($state, $target=0){
         switch($state){
@@ -94,6 +94,6 @@ class lmPasswordEngine implements \LoginMaster\PasswordEngine{
         return $input==$database;
     }
 };
-$lm=new \LoginMaster\LoginMaster($config, new lmHandler(), new lmPasswordEngine(), new \LoginMaster\defaultTwoFactor());
+$lm=new \LoginMaster\LoginMaster($lmconfig, new lmHandler(), new lmPasswordEngine(), new \LoginMaster\defaultTwoFactor());
 $lm->init();
 */
